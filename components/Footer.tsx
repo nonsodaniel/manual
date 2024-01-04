a3cd5bfff4628b8e5750086d94136a1c2a07dad7
nonsodaniel <nonsodaniel07@gmail.com>
2024-01-04 15:30:24 +0100

    Feat:Created footer page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,151 +1,86 @@
-import { footerCompanyList, footerInfoList, footerProductList } from "@/lib/db";
+import {
+  footerCompanyList,
+  footerInfoList,
+  footerProductList,
+  footerSocialLogos,
+} from "@/lib/db";
 import Link from "next/link";
 import React from "react";
+import Logo from "./Logo";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="text-[#0b3b3c]">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-b border-gray-200">
           <div className="sm:col-span-12 lg:col-span-3">
             <div className="mb-2">
-              {/* Logo */}
-              <Link href="/" className="inline-block" aria-label="Cruip">
-                <svg
-                  className="w-8 h-8"
-                  viewBox="0 0 32 32"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <defs>
-                    <radialGradient
-                      cx="21.152%"
-                      cy="86.063%"
-                      fx="21.152%"
-                      fy="86.063%"
-                      r="79.941%"
-                      id="footer-logo"
-                    >
-                      <stop stopColor="#4FD1C5" offset="0%" />
-                      <stop stopColor="#81E6D9" offset="25.871%" />
-                      <stop stopColor="#338CF5" offset="100%" />
-                    </radialGradient>
-                  </defs>
-                  <rect
-                    width="32"
-                    height="32"
-                    rx="16"
-                    fill="url(#footer-logo)"
-                    fillRule="nonzero"
-                  />
-                </svg>
+              <Link href="/" className="inline-block" aria-label="manual-logo">
+                <Logo />
               </Link>
             </div>
           </div>
 
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-gray-800 font-medium mb-2">PRODUCT</h6>
+            <h6 className="font-medium mb-2">PRODUCT</h6>
             <ul className="text-sm">
               {footerProductList.map((item) => (
                 <li className="mb-2" key={item.label}>
-                  <Link
-                    href="#"
-                    className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
-                  >
-                    {item.label}
-                  </Link>
+                  <Link href="#">{item.label}</Link>
                 </li>
               ))}
             </ul>
           </div>
 
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-gray-800 font-medium mb-2">COMPANY</h6>
+            <h6 className="font-medium mb-2">COMPANY</h6>
             <ul className="text-sm">
               {footerCompanyList.map((item) => (
                 <li className="mb-2" key={item.label}>
-                  <Link
-                    href="#"
-                    className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
-                  >
-                    {item.label}
-                  </Link>
+                  <Link href="#">{item.label}</Link>
                 </li>
               ))}
             </ul>
           </div>
 
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-gray-800 font-medium mb-2">INFO</h6>
+            <h6 className="font-medium mb-2">INFO</h6>
             <ul className="text-sm">
               {footerInfoList.map((item) => (
                 <li className="mb-2" key={item.label}>
-                  <Link
-                    href="#"
-                    className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
-                  >
-                    {item.label}
-                  </Link>
+                  <Link href="#">{item.label}</Link>
                 </li>
               ))}
             </ul>
           </div>
 
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-3">
-            <h6 className="text-gray-800 font-medium mb-2">Subscribe</h6>
+            <h6 className="font-medium mb-2">Follow Us</h6>
             <div className="md:flex md:items-center md:justify-between  ">
-              {/* Social links */}
               <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
-                <li>
-                  <Link
-                    href="#"
-                    className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out"
-                    aria-label="Twitter"
-                  >
-                    <svg
-                      className="w-8 h-8 fill-current"
-                      viewBox="0 0 32 32"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M24 11.5c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4 0 1.6 1.1 2.9 2.6 3.2-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H8c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4c.7-.5 1.3-1.1 1.7-1.8z" />
-                    </svg>
-                  </Link>
-                </li>
-                <li className="ml-4">
-                  <Link
-                    href="#"
-                    className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out"
-                    aria-label="Github"
-                  >
-                    <svg
-                      className="w-8 h-8 fill-current"
-                      viewBox="0 0 32 32"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z" />
-                    </svg>
-                  </Link>
-                </li>
-                <li className="ml-4">
-                  <Link
-                    href="#"
-                    className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out"
-                    aria-label="Facebook"
-                  >
-                    <svg
-                      className="w-8 h-8 fill-current"
-                      viewBox="0 0 32 32"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M14.023 24L14 17h-3v-3h3v-2c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V14H21l-1 3h-2.72v7h-3.257z" />
-                    </svg>
-                  </Link>
-                </li>
+                {footerSocialLogos.map((logo) => (
+                  <li key={logo.label}>
+                    <Link href="#">
+                      <Image
+                        className="p-2"
+                        src={logo.path}
+                        alt={logo.label}
+                        width={40}
+                        height={40}
+                        sizes="100vh"
+                      />
+                    </Link>
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
         </div>
       </div>
-      <p className="text-center py-5">2021 Manual. All rights reserved</p>
+      <p className="text-center py-5">
+        {new Date().getFullYear()} Manual. All rights reserved
+      </p>
     </footer>
   );
 };
